a07c36b892f91d5e33226adb64c368384d6b0fb3
aydinSerbest <aydinserbest34@gmail.com>
2024-04-08 20:40:40 +0200

    updated

--- src/test/java/starter/domain/User.java
+++ src/test/java/starter/domain/User.java
@@ -1,4 +1,11 @@
 package starter.domain;
 
-public record User(String emaail, String firstName, String lastName) {
+public record User(String email, String firstName, String lastName) {
 }
+/*
+    1- Cucumber performs field name to column name matching in a case-sensitive manner.
+    2- If the field names in your User class do not exactly match the column names in your feature file,
+    Cucumber will not be able to perform the transformation correctly.
+    3- This class should have fields that match the column headers in the table and the field names
+    should be the same as the column names, case sensitively.
+ */
--- src/test/java/starter/stepdefinitions/ListingUsersStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/ListingUsersStepDefinitions.java
@@ -2,6 +2,7 @@ package starter.stepdefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import starter.domain.User;
 
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,7 @@ public class ListingUsersStepDefinitions {
 
     }
 
-    //we are sending table from feature file as List<Map<String, String>>
+    //we are sending table here from feature file as List<Map<String, String>>
     /*
     Examples:
       | Page | email                      | first_name | last_name |
@@ -28,10 +29,19 @@ public class ListingUsersStepDefinitions {
     ]
 
      */
+//    @Then("the users should include:")
+//    public void userListShouldInclude(List<Map<String, String>> userData){
+//        System.out.println(userData);
+//        // output:  [{email=emma.wong@reqres.in, first_name=Emma, last_name=Wong}]
+//    }
+    /*
+    2. way to send table is using domain object, here we used record object,
+    instead of List<Map<String, String>> userData,
+    we use List<User> users  --> List of User objects
+     */
     @Then("the users should include:")
-    public void userListShouldInclude(List<Map<String, String>> userData){
-        System.out.println(userData);
-        // output:  [{email=emma.wong@reqres.in, first_name=Emma, last_name=Wong}]
+    public void userListShouldInclude(List<User> users){
+        System.out.println(users);
     }
 
 }
